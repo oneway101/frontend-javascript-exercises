a760cbbc5077d4ba3a659eed2377a1cb34aa7231
Ha Na Gill <oneway.first@gmail.com>
2014-12-19 00:00:34 +0000

    objects exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,37 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle,courseDuration,courseStudents){
+	var course = {};
+	course = {title: courseTitle, duration: courseDuration, students: courseStudents };
+	console.log(course);
+	return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object,newProp,newValue){
+	var newObject = object;
 
-module.exports.formLetter = undefined;
+	if (newObject[newProp] != object.newProp){
+		console.log(newObject);
+		return newObject;	
+	}
+	else{
+		newObject[newProp] = newValue;
+		console.log(newObject);
+		return newObject;	
+	}
+	
+	
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter){
+	var letterStr = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+	return letterStr;
+};
+
+module.exports.canIGet = function(item,money){
+	var priceTag = {}
+	priceTag["MacBook Air"] = 999;
+	priceTag["MacBook Pro"] = 1299;
+	priceTag["Mac Pro"] = 2499;
+	priceTag["Apple Sticker"] = 1;
+	return priceTag[item] <= money;
+	
+};
